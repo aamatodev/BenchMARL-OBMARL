b5dba60445da3f0c29c3871e3efb2612d1f2d1ba
Alessandro Amato <aa357@students.uwf.edu>
2024-11-25 17:30:34 -0600

    feat: add objective matching gnn and disperse env specific critic

--- benchmarl/models/objective_matching_gnn.py
+++ benchmarl/models/objective_matching_gnn.py
@@ -41,14 +41,23 @@ def generate_graph(batch_size, node_features, node_pos, edge_attr, n_agents, dev
     else:
         adjacency = torch.ones(n_agents, n_agents, device=device, dtype=torch.long)
         edge_index, _ = torch_geometric.utils.dense_to_sparse(adjacency)
-
         # remove self loops
         graphs.edge_index, _ = torch_geometric.utils.remove_self_loops(edge_index)
+        n_edges = edge_index.shape[1]
+        # Tensor of shape [batch_size * n_edges]
+        # in which edges corresponding to the same graph have the same index.
+        batch = torch.repeat_interleave(b, n_edges)
+        # Edge index for the batched graphs of shape [2, n_edges * batch_size]
+        # we sum to each batch an offset of batch_num * n_agents to make sure that
+        # the adjacency matrices remain independent
+        batch_edge_index = edge_index.repeat(1, batch_size * bc) + batch * n_agents
+        graphs.edge_index = batch_edge_index
 
     graphs = torch_geometric.transforms.Cartesian(norm=False)(graphs)
     graphs = torch_geometric.transforms.Distance(norm=False)(graphs)
 
     return graphs
+
 
 
 class Encoder(nn.Module):
@@ -94,18 +103,20 @@ class ObjectiveMatchingGNN(Model):
 
         self.node_pos_encoder = Encoder(2, 8).to(self.device)
         self.edge_encoder = Encoder(2, 8).to(self.device)
-        self.matching_gnn = GATv2Conv(3, 128, 1, edge_dim=3).to(self.device)
+        self.matching_gnn = GATv2Conv(2, 128, 1, edge_dim=3).to(self.device)
         self.agent_gnn = GATv2Conv(128, 128, 1, edge_dim=3).to(self.device)
         self.Final_encoder = Encoder(128, 9).to(self.device)
 
         self.final_mlp = MultiAgentMLP(
-            n_agent_inputs=128,
+            n_agent_inputs=269,
             n_agent_outputs=self.output_features,
             n_agents=self.n_agents,
             centralised=self.centralised,
             share_params=self.share_params,
             device=self.device,
-            activation_class=self.activation_function
+            activation_class=self.activation_function,
+            depth=3,
+            num_cells=[128, 64, 32],
         )
 
     def _perform_checks(self):
@@ -114,35 +125,48 @@ class ObjectiveMatchingGNN(Model):
     def _forward(self, tensordict: TensorDictBase) -> TensorDictBase:
         # Input has multi-agent input dimension
         if self.input_has_agent_dim:
-            # create the agent - objective graph
+            # create the objective graph
             batch_size = tensordict.get("agents")["observation"]["agent_pos"].shape[:-2][0]
 
+            # reorganize the landmark positions for the matching GNN
             landmark_positions = tensordict.get("agents")["observation"]["landmark_pos"]
-            landmark_positions = landmark_positions.view(-1, 2)
+            # Step 1: Keep only the first element in the second dimension
+            landmark_positions = landmark_positions[:, :1, :]  # Shape: [10, 1, 10]
+            # Step 2: Remove the first two elements in the third dimension
+            landmark_positions = landmark_positions[:, :, 2:]  # Shape: [10, 1, 8]
+            # Step 3: Reshape to [10, 4, 2]
 
-            landmark_eaten = tensordict.get("agents")["observation"]["landmark_eaten"]
-            landmark_eaten = landmark_eaten.view(-1, 1)
+            landmark_positions = landmark_positions.reshape(-1, 2)
 
-            node_features = torch.cat([landmark_positions, landmark_eaten], dim=1)
+            # Commenting this out given that we don't want it in the initial objetive graph
+            # [:, :, 1:] to remove the first column which is the agent status
+            # landmark_eaten = tensordict.get("agents")["observation"]["landmark_eaten"][:, :, 1:]
+            # landmark_eaten = landmark_eaten.view(-1, 1)
+            # node_features = torch.cat([landmark_positions, landmark_eaten], dim=1)
 
-            # Agent-Objective graph representation
-            graphs = generate_graph(batch_size, node_features, landmark_positions, None, self.n_agents + 1, self.device,
-                                    bc=self.n_agents)
+            # Objective graph representation
+            graphs = generate_graph(batch_size, landmark_positions, landmark_positions, None, self.n_agents, self.device)
             h1 = F.relu(self.matching_gnn(x=graphs.x, edge_index=graphs.edge_index, edge_attr=graphs.edge_attr))
-
-            # get the agents observation
-            agent_objective_embedding_post_gnn = h1[0::5]
 
             # create the agent - agent graph
             agent_positions = tensordict.get("agents")["observation"]["agent_pos"]
 
-            graphs = generate_graph(batch_size, agent_objective_embedding_post_gnn, agent_positions.view(-1, 2), None,
-                                    self.n_agents, self.device, use_radius=True)
-
+            graphs = generate_graph(batch_size,  agent_positions.view(-1, 2), agent_positions.view(-1, 2), None,
+                                    self.n_agents, self.device)
             # Agent-Agent graph representation
-            h2 = F.relu(self.agent_gnn(x=graphs.x, edge_index=graphs.edge_index, edge_attr=graphs.edge_attr))
+            h2 = F.relu(self.matching_gnn(x=graphs.x, edge_index=graphs.edge_index, edge_attr=graphs.edge_attr))
 
-            res = self.final_mlp.forward(h2.view(batch_size, self.n_agents, -1))
+            # Get cosine similarity between agent and objective graph
+            cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+            agent_objective_similarity = cos(h1, h2)
+
+            # Concatenate the agent-objective similarity to the agent-objective graph
+            agent_final_obs = torch.cat([h1.reshape(batch_size, self.n_agents, -1),
+                                         h2.reshape(batch_size, self.n_agents, -1),
+                                         agent_objective_similarity.reshape(batch_size, self.n_agents, -1),
+                                         tensordict.get("agents")["observation"]["relative_landmark_pos"]], dim=2)
+
+            res = self.final_mlp.forward(agent_final_obs)
 
         tensordict.set(self.out_key, res)
         return tensordict
